5c36927088a3f2fc9907035138526419641538e7
Ajay kumar <ajcoder25@gmail.com>
2024-01-06 19:47:19 +0530

    Third commit

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-import LocomotiveScroll from 'locomotive-scroll';
+import LocomotiveScroll from 'locomotive-scroll/dist/locomotive-scroll.esm.min.js';
+
 
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+
+// var a = document.querySelector("#elem1")
+// var image = a.getAttribute("data-image")
+// console.log(image);
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var elemC = document.getElementById("elem-container");
+
+    elemC.addEventListener("mouseenter", function () {
+        alert('this is AJay');
+    });
+});
+
+
+// document.getElementById("elem-container").addEventListener("mouseenter", function () {
+//     alert('this is AJay');
+// });
+
