37f224e618b6a76be5fab4096a95dca76d4c677c
Ajay kumar <ajcoder25@gmail.com>
2024-01-07 20:48:24 +0530

    Created the 5th Page

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-import LocomotiveScroll from 'locomotive-scroll/dist/locomotive-scroll.esm.min.js';
+// import LocomotiveScroll from 'locomotive-scroll/dist/locomotive-scroll.esm.min.js';
 
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
 
 
 // var a = document.querySelector("#elem1")
@@ -13,16 +13,45 @@ const scroll = new LocomotiveScroll({
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    var elemC = document.getElementById("elem-container");
+// document.addEventListener("DOMContentLoaded", function () {
+//     var elemC = document.getElementById("elem-container");
 
-    elemC.addEventListener("mouseenter", function () {
-        alert('this is AJay');
-    });
-});
+//     elemC.addEventListener("mouseenter", function () {
+//         alert('this is AJay');
+//     });
+// });
 
 
 // document.getElementById("elem-container").addEventListener("mouseenter", function () {
 //     alert('this is AJay');
 // });
+
+// var elems = document.querySelectorAll(".elem")
+// elems.forEach(function (e) {
+//     e.addEventListener("mouseenter", function () {
+//         var image = e.getAttribute("data-image")
+//         fixed.style.backgroundImage = `url(${image})`
+//     })
+// });
+
+var elemC = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+
+elemC.addEventListener("mouseenter", function () {
+    fixed.style.display = "block";
+})
+
+elemC.addEventListener("mouseleave", function () {
+    fixed.style.display = "none";
+})
+
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+        var image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
+
 
